2d445b8707028da20d723d02b8cfc61346397ed6
DiegoTorresMijarra <diegotorresmijarra@gmail.com>
2023-12-20 18:26:53 +0100

    feat(controlador snake)

---
+++ src/main/java/api/controlador/Movimiento.java
@@ -0,0 +1,14 @@
+package api.controlador;
+
+/**
+ * enumeracion que referencia los posibles movimientos que puede hace la serpiente
+ * @author Diego
+ *
+ */
+public enum Movimiento {
+	arriba,
+	abajo,
+	derecha,
+	izquierda,
+	parado
+}
